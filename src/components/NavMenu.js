7f26690c31862ae6d01aca36be39915c6515db7d
Felipe :D <112602535+lfelipeu@users.noreply.github.com>
2022-12-07 13:31:18 -0600

    Update NavMenu.js

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -19,17 +19,6 @@ export default class NavMenu extends React.Component {
     }
     render() {
         return (
-            <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light >
-            <Container>
-            <NavbarBrand tag={Link} to="/"></NavbarBrand>
-            <NavbarToggler onClick={this.toggle} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={this.state.isOpen} navbar>
-            <ul className="navbar-nav flex-grow">
-                <NavItem><NavLink className="text-dark" href="#"></NavLink></NavItem>
-            </ul>
-            </Collapse>
-            </Container>
-            </Navbar>
     );
 }
 }
